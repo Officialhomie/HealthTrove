0647956e43fdeb5e19680bea820febcdf69693b6
OneTrueHomie <vicigwilo@gmail.com>
2024-10-17 10:44:41 +0100

    fixed some imports

--- src/pinata.ts
+++ src/pinata.ts
@@ -1,6 +1,11 @@
 import { PinataSDK } from "pinata-web3";
 
+const PINATA_JWT = import.meta.env.VITE_PINATA_JWT
+const pinataGateway = import.meta.env.VITE_PINATA_GATEWAY
+
 const pinata = new PinataSDK({
-  pinataJwt: "PINATA_JWT",
-  pinataGateway: "example-gateway.mypinata.cloud",
+  pinataJwt: PINATA_JWT,
+  pinataGateway: pinataGateway,
 });
+
+export default pinata;
